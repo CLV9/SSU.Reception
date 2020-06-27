6656728e1d83f499c4b2a7c59441eb80c8c759e0
Andriyanov Denis <andriyanovdenis99@gmail.com>
2020-06-27 14:52:24 +0400

    Restructure

--- SSU.Reception/SSU.Reception2/App_Start/BundleConfig.cs
+++ SSU.Reception/SSU.Reception2/App_Start/BundleConfig.cs
@@ -21,9 +21,9 @@ namespace SSU.Reception
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Style/CssBundle").Include(
+					  "~/Style/bootstrap.css",
+					  "~/Style/Site.css"));
 		}
 	}
 }
